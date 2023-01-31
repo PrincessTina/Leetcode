09218627bdc7a29036f6e60def949d51a9f0ba01
Kristina <kristina.orl-va@bk.ru>
2023-02-01 01:28:12 +0300

    Time: 14 ms (33.80%), Space: 14.3 MB (8.89%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& Operations) {
+        unordered_map<string, int> AllOperations;
         int X = 0;
         
+        AllOperations.insert({"--X", -1});
+        AllOperations.insert({"X--", -1});
+        AllOperations.insert({"++X", 1});
+        AllOperations.insert({"X++", 1});
+        
         for (string Operation: Operations)
+        {
+            X += AllOperations[Operation];
+        }
+        
+        /*for (string Operation: Operations)
         {
             if (Operation == "--X" || Operation == "X--")
             {
@@ -13,7 +24,7 @@ public:
             {
                 X++;
             }
-        }
+        }*/
         
         return X;
     }
