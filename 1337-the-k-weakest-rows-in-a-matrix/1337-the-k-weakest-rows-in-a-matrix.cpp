a3ec10793f8e1e884a9ae41e82a0e2f02f1d574a
Kristina <kristina.orl-va@bk.ru>
2023-01-31 15:33:17 +0300

    Time: 11 ms (89.62%), Space: 10.5 MB (44.97%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& Matrix, int k) 
     {
-        // vector<int> Values;
-        // vector<int> Indexes;
-        // vector<int> Result;
-        vector<pair<int, int>> Map;
         vector<int> Result;
+        vector<pair<int, int>> Map;
         
         for (int i = 0; i < Matrix.size(); i++)
         {
@@ -25,7 +22,6 @@ public:
             }
             
             Map.push_back({i, SoldiersCount});
-            //Insert(SoldiersCount, i, Values, Indexes);
         }
         
         sort(Map.begin(), Map.end(), [](pair<int, int> Value1, pair<int, int> Value2) 
@@ -44,66 +40,5 @@ public:
         }
         
         return Result;
-    }
-    
-private:
-    void Insert(int Value, int Index, vector<int>& Values, vector<int>& Indexes)
-    {
-        if (Values.empty())
-        {
-            Values.push_back(Value);
-            Indexes.push_back(Index);
-            
-            return;
-        }
-        
-        for (int i = Values.size() - 1; i >= 0; i--)
-        {
-            if (Value > Values[i])
-            {
-                if (i == Values.size() - 1)
-                {
-                    Values.push_back(Value);
-                    Indexes.push_back(Index);
-                }
-                else
-                {
-                    Values.emplace(Values.begin() + i + 1, Value);
-                    Indexes.emplace(Indexes.begin() + i + 1, Index);
-                }
-                
-                break;
-            }
-            
-            if (Value == Values[i])
-            {
-                if (Index > Indexes[i])
-                {
-                    if (i == Values.size() - 1)
-                    {
-                        Values.push_back(Value);
-                        Indexes.push_back(Index);
-                    }
-                    else
-                    {
-                        Values.emplace(Values.begin() + i + 1, Value);
-                        Indexes.emplace(Indexes.begin() + i + 1, Index);
-                    }
-                }
-                else
-                {
-                    Values.emplace(Values.begin() + i, Value);
-                    Indexes.emplace(Indexes.begin() + i, Index);
-                }
-                
-                break;
-            }
-            
-            if (i == 0)
-            {
-                Values.emplace(Values.begin(), Value);
-                Indexes.emplace(Indexes.begin(), Index);
-            }
-        }
     }
 };
