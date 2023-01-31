3f4b40fdabede18c00f7e9b6c2959d1ba76dd54c
Kristina <kristina.orl-va@bk.ru>
2023-01-31 19:43:36 +0300

    Time: 3 ms (48.92%), Space: 5.8 MB (92.03%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t Num) {
+        int OnesCount = 0;
+        
+        while (Num != 0)
+        {
+            if (Num % 2 == 1)
+            {
+                OnesCount++;
+            }
+            
+            Num /= 2;
+        }
+        
+        return OnesCount;
+    }
+};
