8aee120b1a9a7f14d2c0471aa08bbdc0b6747886
Kristina <kristina.orl-va@bk.ru>
2023-02-02 23:33:34 +0300

    Time: 7 ms (24.88%), Space: 9.2 MB (34.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,48 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& Nums1, int m, vector<int>& Nums2, int n) {
-        int InsertionIndex = 0;
-        
-        for (int j = 0; j < n; j++)
+        for (int i = m; i < Nums1.size(); i++)
         {
-            int Num = Nums2[j];
-            
-            // Find insertion index
-            while (true)
-            {
-                if (Num < Nums1[InsertionIndex])
-                {
-                    break;
-                }
-                
-                if (Nums1[InsertionIndex] == 0 && InsertionIndex >= m + j)
-                {
-                    break;
-                }
-                
-                if (InsertionIndex + 1 >= Nums1.size())
-                {
-                    break;
-                }
-                
-                InsertionIndex++;
-            }
-            
-            // Move elements to the right
-            int Replacement = Nums1[InsertionIndex];
-            Nums1[InsertionIndex] = Num;
-            
-            for (int i = InsertionIndex + 1; i < Nums1.size(); i++)
-            {
-                if (Replacement == 0 && InsertionIndex >= m + j)
-                {
-                    break;
-                }
-                
-                const int Temp = Nums1[i];
-                Nums1[i] = Replacement;
-                Replacement = Temp;
-            }
+            Nums1[i] = Nums2[i - m];
         }
+        
+        sort(Nums1.begin(), Nums1.end());
     }
 };
