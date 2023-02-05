528f44c6e5ea2bd589fe733a3f9947bb79f72ff6
Kristina <kristina.orl-va@bk.ru>
2023-02-06 00:55:00 +0300

    Time: 0 ms (100.00%), Space: 6.6 MB (91.69%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string S1, string S2) {
-        char Result = '\0';
+        char Result = S2[S2.size() - 1];
         
         for (int i = 0; i < S1.size(); i++)
         {
             Result = Result xor S1[i] xor S2[i];
         }
         
-        return Result xor S2[S2.size() - 1];
+        return Result;
     }
 };
