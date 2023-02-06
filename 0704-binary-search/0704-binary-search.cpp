32954802ed905a6215059e543becf7424d7b3079
Kristina <kristina.orl-va@bk.ru>
2023-02-07 01:53:33 +0300

    Time: 37 ms (81.74%), Space: 27.6 MB (17.14%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& Nums, int Target) {
+        int LeftBorder = 0;
+        int RightBorder = Nums.size() - 1;
+        int Center;
+        
+        while (LeftBorder != RightBorder && RightBorder >= 0)
+        {
+            Center = (LeftBorder + RightBorder) / 2;
+            
+            if (Target < Nums[Center])
+            {
+                RightBorder = Center - 1;
+                continue;
+            }
+            
+            if (Target >= Nums[Center + 1])
+            {
+                LeftBorder = Center + 1;
+                continue;
+            }
+            
+            return Target == Nums[Center] ? Center : -1;
+        }
+        
+        return Target == Nums[LeftBorder] ? LeftBorder : -1;
+    }
+};
