44b0bf996757a718e6d9437e48002aa646dfa2fa
Kristina <kristina.orl-va@bk.ru>
2023-02-07 18:38:56 +0300

    Time: 137 ms (89.30%), Space: 66.2 MB (89.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int totalFruit(vector<int>& Fruits) {
-        Fruit Basket1 = {-1, 0, 0};
+        Fruit Basket1 = {Fruits[0], 0, 0};
         Fruit Basket2 = {-1, 0, 0};
         int MaxFruits = 0;
         
-        // 1 0 1 4 1 4 1 2 3
-        // 1 1 2 3
-        // Третье решение
         for (int i = 0; i < Fruits.size(); i++)
         {
-            if (Basket1.Type == -1)
-            {
-                Basket1 = {Fruits[i], 1, 1};
-                Basket2.Streak = 0;
-                continue;
-            }
-            
             if (Basket1.Type == Fruits[i])
             {
                 Basket1.Count++;
@@ -61,87 +51,6 @@ public:
         {
             MaxFruits = Basket1.Count + Basket2.Count;
         }
-        
-        // Второе решение
-        /*for (int i = 0; i < Fruits.size(); i++)
-        {
-            for (int j = i; j < Fruits.size(); j++)
-            {
-                if (Basket1.first == -1)
-                {
-                    Basket1 = {Fruits[j], 1};
-                    continue;
-                }
-                
-                if (Basket1.first == Fruits[j])
-                {
-                    Basket1.second++;
-                    continue;
-                }
-
-                if (Basket2.first == -1)
-                {
-                    Basket2 = {Fruits[j], 1};
-                    continue;
-                }
-
-                if (Basket2.first == Fruits[j])
-                {
-                    Basket2.second++;
-                    continue;
-                }
-
-                break;
-            }
-            
-            if (Basket1.second + Basket2.second > MaxFruits)
-            {
-                MaxFruits = Basket1.second + Basket2.second;
-            }
-
-            Basket1 = {-1, 0};
-            Basket2 = {-1, 0};
-        }
-        
-        if (Basket1.second + Basket2.second > MaxFruits)
-        {
-            MaxFruits = Basket1.second + Basket2.second;
-        }*/
-        
-        // Первое решение
-        /*for (int Fruit: Fruits)
-        {
-            if (Basket1.first == Fruit)
-            {
-                Basket1.second++;
-                continue;
-            }
-            
-            if (Basket2.first == -1)
-            {
-                Basket2 = {Fruit, 1};
-                continue;
-            }
-            
-            if (Basket2.first == Fruit)
-            {
-                Basket2.second++;
-                continue;
-            }
-            
-            if (Basket1.second + Basket2.second > MaxFruits)
-            {
-                MaxFruits = Basket1.second + Basket2.second;
-            }
-                
-            Basket1 = Basket2;
-            Basket2 = {Fruit, 1};
-        }
-        
-        if (Basket1.second + Basket2.second > MaxFruits)
-        {
-            MaxFruits = Basket1.second + Basket2.second;
-        }*/
         
         return MaxFruits;
     }
