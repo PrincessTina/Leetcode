87257380f8cb24b61def66768a15cb7a17a18062
Kristina <kristina.orl-va@bk.ru>
2023-02-11 23:12:23 +0300

    Time: 6 ms (21.20%), Space: 8.4 MB (80.09%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* Root) {
+        TreeNode* Node = Root;
+        stack<TreeNode*> Stack;
+        vector<int> Result;
+        
+        while (Node != nullptr)
+        {
+            Result.push_back(Node->val);
+            
+            if (Node->left != nullptr)
+            {
+                if (Node->right != nullptr)
+                {
+                    Stack.push(Node->right);
+                }
+                
+                Node = Node->left;
+                continue;
+            }
+            
+            if (Node->right != nullptr)
+            {
+                Node = Node->right;
+                continue;
+            }
+            
+            if (!Stack.empty())
+            {
+                Node = Stack.top();
+                Stack.pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        /*while (Node != nullptr)
+        {
+            Stack.push(Node);
+            Result.push_back(Node->val);
+            
+            if (Node->left != nullptr)
+            {
+                Node = Node->left;
+                continue;
+            }
+            
+            Stack.pop();
+            
+            if (Node->right != nullptr)
+            {
+                Node = Node->right;
+                continue;
+            }
+            
+            if (!Stack.empty())
+            {
+                Node = Stack.top();
+                continue;
+            }
+            
+            break;
+        }*/
+        
+        return Result;
+    }
+};
