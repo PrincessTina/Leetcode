71e421624133e4941c9acf160379568f807e0857
Kristina <kristina.orl-va@bk.ru>
2023-02-11 03:40:32 +0300

    Time: 3 ms (37.90%), Space: 6 MB (42.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        const int MaxTwoCount = n / 2;
+        long long Fraction = n - 1;
+        int Ways = n;
+        
+        for (int i = 2; i <= MaxTwoCount; i++)
+        {
+            Fraction = Fraction * (n - i) * (n - i - 1) / ((n - 1) * i);
+            
+            Ways += Fraction;
+            n -= 1;
+        }
+        
+        return Ways;
+    }
+};
