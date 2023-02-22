c7fe58cc50cc9e43506ec23057c043f2caf9623f
Kristina <kristina.orl-va@bk.ru>
2023-02-23 01:38:17 +0300

    Time: 968 ms (5.18%), Space: 26.2 MB (11.25%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& Weights, int Days) {
+        int Sum = 0;
+        
+        for (int Weight: Weights)
+        {
+            Sum += Weight;
+        }
+        
+        for (int i = 1; i <= Sum; i++)
+        {
+            if (CheckWeight(Weights, Days, i))
+            {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+    
+private:
+    bool CheckWeight(vector<int>& Weights, int Days, int MaxWeight)
+    {
+        int Count = 1;
+        int Sum = 0;
+        
+        for (int Weight: Weights)
+        {
+            if (Sum + Weight <= MaxWeight)
+            {
+                Sum += Weight;
+                continue;
+            }
+            
+            if (Weight > MaxWeight)
+            {
+                return false;
+            }
+            
+            Sum = Weight;
+            Count++;
+            
+            if (Count > Days)
+            {
+                return false;
+            }
+        }
+        
+        return Count <= Days;
+    }
+};
+
+/*
+1 2 3  4  5  6  7  8  9 10
+1 3 6 10 15 21 28 36 45 55
+
+1 2 3 1 1
+1 3 6 7 8
+*/
