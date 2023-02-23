15efe62c1c4840e57e0f866ab4d7e627f94d3cba
Kristina <kristina.orl-va@bk.ru>
2023-02-23 18:41:01 +0300

    Time: 215 ms (89.67%), Space: 82.1 MB (53.52%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -18,13 +18,6 @@ public:
         int Count = 0;
         int Iterator = 0;
         
-        /*
-        0 1 1 2 2 3 4 12
-        1 6 2 0 3 2 5 10
-    
-        0,1 1,6 1,2 2,0 2,3 3,2 4,5 12,10
-        */
-        
         while (Count < k)
         {
             for (int i = Iterator; i < Projects.size(); i++)
@@ -56,50 +49,6 @@ public:
             Count++;
         }
         
-        /*for (auto El: Temp)
-        {
-            Projects.push_back(Project(El.first, El.second));
-            make_heap(Projects[Projects.size() - 1].Profits.begin(), Projects[Projects.size() - 1].Profits.end());
-        }
-        
-        int Count = 0;
-        while (Count < k)
-        {
-            const int Previous = Count;
-            
-            for (Project& TProject: Projects)
-            {
-                if (TProject.Capital <= Capital && TProject.Profits.size() > 0)
-                {
-                    Capital += TProject.Profits.front();
-                    Count++;
-                    
-                    pop_heap(TProject.Profits.begin(), TProject.Profits.end());
-                    TProject.Profits.pop_back();
-                    
-                    break;
-                }
-            }
-            
-            if (Count == Previous)
-            {
-                break;
-            }
-        }*/
-        
         return Capital;
     }
-    
-private:
-    /*0 1 1
-    1 3 2
-    
-    0 1 1 2 2 3 4 12
-    1 6 2 0 3 2 5 10
-    
-    0,1 1,6 1,2 2,0 2,3 3,2 4,5 12,10
-    
-    12 1 4 2 3 1 0 2
-    10 6 5 4 2 2 1 0
-    */
 };
