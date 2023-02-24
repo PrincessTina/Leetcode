b3cb96a23f09b5ad09ece4d0ad5c320760930e55
Kristina <kristina.orl-va@bk.ru>
2023-02-24 05:28:50 +0300

    Time: 306 ms (94.92%), Space: 82 MB (43.22%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& Nums) {
         set<int> Set;
-        int Deviation;
         
         for (int Num: Nums)
         {
             Set.insert(Num);
         }
         
-        Deviation = *Set.rbegin() - *Set.begin();
+        int Deviation = *Set.rbegin() - *Set.begin();
         
         while (*Set.begin() % 2 != 0)
         {
@@ -35,27 +34,4 @@ public:
         
         return Deviation;
     }
-    
-    /* 
-    1 3 4 5 20
-    1 3 4 5 10
-    1 3 4 5
-    2 3 4 5
-    
-    2 8 10
-    2 5 8
-    2 4 5
-    
-    1 2 3 4
-    1 2 3
-    2 3
-    
-    3 4 10
-    3 4 5
-    4 5 6
-    
-    3 5
-    5 6
-    6 10
-    */
 };
