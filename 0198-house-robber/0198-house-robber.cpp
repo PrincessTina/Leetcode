0e18fd4806acfe05bff880d0f07c8c13fd1996c9
Kristina <kristina.orl-va@bk.ru>
2023-02-27 05:36:13 +0300

    Time: 2 ms (49.27%), Space: 7.6 MB (97.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int rob(vector<int>& Capitals) {
+        int Loot = 0;
+        
+        for (int i = 0; i < Capitals.size(); i++)
+        {
+            if (i > 1)
+            {
+                if (i > 2 && Capitals[i - 3] > Capitals[i - 2])
+                {
+                    Capitals[i] += Capitals[i - 3];
+                }
+                else
+                {
+                    Capitals[i] += Capitals[i - 2];
+                }
+                
+                //Capitals[i] += Capitals[i - 2];
+            }
+            
+            if (Capitals[i] > Loot)
+            {
+                Loot = Capitals[i];
+            }
+        }
+        
+        return Loot;
+        
+        /*
+        2 1 3 3
+        */
+    }
+};
