0c1a7309bf7f01aabf2c97f42c96ae4db1363324
Kristina <kristina.orl-va@bk.ru>
2023-03-01 22:32:16 +0300

    Time: 7 ms (71.31%), Space: 12.4 MB (12.55%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> letterCasePermutation(string String) {
+        MakeString(String, 0);
+        return Result;
+    }
+    
+private:
+    vector<string> Result;
+    char Diff = 'A' - 'a';
+    
+    void MakeString(string String, int i)
+    {
+        while (i < String.size())
+        {
+            if (String[i] >= 'a' && String[i] <= 'z')
+            {
+                MakeString(String, i + 1);
+                String[i] += Diff;
+                MakeString(String, i + 1);
+                
+                break;
+            }
+            
+            if (String[i] >= 'A' && String[i] <= 'Z')
+            {
+                MakeString(String, i + 1);
+                String[i] -= Diff;
+                MakeString(String, i + 1);
+                
+                break;
+            }
+            
+            i++;
+        }
+        
+        if (i == String.size())
+        {
+            Result.push_back(String);
+        }
+    }
+};
