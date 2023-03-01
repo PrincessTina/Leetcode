4b9b37252fa332e5595ba7ded6da0fb3efdadd7f
Kristina <kristina.orl-va@bk.ru>
2023-03-02 00:15:27 +0300

    Time: 7 ms (98.54%), Space: 10 MB (57.14%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,31 +2,20 @@ class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<int> Array(k);
+        MakeArray(Array, 0, 1);
         
-        Max = n;
-        MakeArray(Array, 1, 0);
-        MakeCombination(Array);
-        
-        return Result;
-    }
-    
-private:
-    vector<vector<int>> Result;
-    int Max;
-    
-    void MakeCombination(vector<int>& Array)
-    {
         while (true)
         {
-            while (Array[Array.size() - 1] < Max)
+            int i = Array.size() - 1;
+            
+            while (Array[i] < n - Array.size() + 1 + i)
             {
-                Array[Array.size() - 1]++;
-                Result.push_back(Array);
+                MakeArray(Array, i, Array[i] + 1);
             }
             
-            int i = Array.size() - 2;
+            i = Array.size() - 2;
             
-            while (i >= 0 && Array[i] >= Max - Array.size() + 1 + i)
+            while (i >= 0 && Array[i] >= n - Array.size() + 1 + i)
             {
                 i--;
             }
@@ -36,11 +25,16 @@ private:
                 break;
             }
             
-            MakeArray(Array, Array[i] + 1, i);
+            MakeArray(Array, i, Array[i] + 1);
         }
+        
+        return Result;
     }
     
-    void MakeArray(vector<int>& Array, int Value, int From)
+private:
+    vector<vector<int>> Result;
+    
+    void MakeArray(vector<int>& Array, int From, int Value)
     {
         for (int i = From; i < Array.size(); i++)
         {
