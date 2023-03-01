8ac638f82976f53d64a0e80728c83a301ef12bd6
Kristina <kristina.orl-va@bk.ru>
2023-03-01 14:01:17 +0300

    Time: 23 ms (59.21%), Space: 17 MB (56.43%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -9,21 +9,5 @@ public:
         }
         
         return Result;
-        
-        /*unordered_set<int> Set;
-        
-        for (int Num: Nums)
-        {
-            if (Set.count(Num))
-            {
-                Set.erase(Num);
-            }
-            else
-            {
-                Set.insert(Num);
-            }
-        }
-        
-        return *Set.begin();*/
     }
 };
