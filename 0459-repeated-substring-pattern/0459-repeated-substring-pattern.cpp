ff1f09677299e62133a6a6b5dc125b1ecff0ccb4
Kristina <kristina.orl-va@bk.ru>
2023-03-03 04:49:01 +0300

    Time: 20 ms (73.18%), Space: 9.5 MB (95.18%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string String) {
-        bool IsRepeating = false;
         int Length = 1;
-        int i = 1;
-        int n = 1;
         
         while (Length < String.size())
         {
@@ -14,74 +11,29 @@ public:
                 continue;
             }
             
-            IsRepeating = true;
-            n = 1;
+            int n = 1;
             
             while (n < String.size() / Length)
             {
-                for (int j = 0; j < Length; j++)
+                for (int i = 0; i < Length; i++)
                 {
-                    if (String[j] == String[n * Length + j])
+                    if (String[i] == String[n * Length + i])
                     {
                         continue;
                     }
                     
-                    IsRepeating = false;
-                    break;
-                }
-                
-                if (!IsRepeating)
-                {
-                    break;
+                    goto Cycle;
                 }
                 
                 n++;
             }
             
-            if (IsRepeating)
-            {
-                return true;
-            }
+            return true;
             
+        Cycle:
             Length++;
         }
         
         return false;
-        
-        /*bool IsRepeating = false;
-        int Length = 1;
-        int i = 1;
-        
-        while (i < String.size())
-        {
-            if (String[i] != String[0])
-            {
-                i++;
-                Length++;
-                IsRepeating = false;
-                continue;
-            }
-            
-            IsRepeating = true;
-            
-            for (int j = 0; j < Length; j++)
-            {
-                if (String[j] == String[i + j])
-                {
-                    continue;
-                }
-                
-                Length++;
-                IsRepeating = false;
-                break;
-            }
-            
-            i += IsRepeating ? Length : 1;
-        }
-        
-        return IsRepeating;*/
     }
-    
-    //bbbcbbbc
-    //bbcbbbcb
 };
