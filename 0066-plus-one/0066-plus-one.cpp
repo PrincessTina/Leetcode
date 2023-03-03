c5560f2769cb546fec62271d403ba7a37ab5a583
Kristina <kristina.orl-va@bk.ru>
2023-03-04 01:38:22 +0300

    Time: 3 ms (56.52%), Space: 8.8 MB (18.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& Digits) {
+        int i = Digits.size() - 1;
+        
+        while (true)
+        {
+            if (Digits[i] < 9)
+            {
+                Digits[i] += 1;
+                break;
+            }
+            
+            Digits[i] = 0;
+            
+            if (i == 0)
+            {
+                Digits.insert(Digits.begin(), 1);
+                break;
+            }
+            
+            i--;
+        }
+        
+        return Digits;
+    }
+};
