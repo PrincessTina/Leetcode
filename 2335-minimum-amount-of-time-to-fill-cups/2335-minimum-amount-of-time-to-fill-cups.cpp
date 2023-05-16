8cf7b424433b5bacd8d66eab6fc2f8083e793883
Kristina <kristina.orl-va@bk.ru>
2023-05-16 23:17:30 +0300

    Time: 5 ms (39.81%), Space: 11.7 MB (92.54%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -21,6 +21,7 @@ public:
             
             Cups.push_back(FirstCup - 1);
             push_heap(Cups.begin(), Cups.end());
+            
             Cups.push_back(SecondCup != 0 ? SecondCup - 1 : 0);
             push_heap(Cups.begin(), Cups.end());
             
@@ -28,28 +29,5 @@ public:
         }
         
         return Steps;
-        
-        // 4, 2, 1
-        
-        // 5, 4, 4
-        
-        // 4,4,3
-        // 3,3,3
-        // 2,2,3
-        // 2,1,2
-        // 1,1,1
-        // 0,0,1
-        // 0,0,0
-        
-        // 5,4,4
-        
-        // 4,3,4
-        // 3,2,4
-        // 2,1,4
-        // 1,0,4
-        // 0,0,3
-        // 0,0,2
-        // 0,0,1
-        // 0,0,0
     }
 };
