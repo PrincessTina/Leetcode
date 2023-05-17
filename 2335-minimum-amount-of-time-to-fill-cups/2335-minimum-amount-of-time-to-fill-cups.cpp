66296f1f11adf097b6d131c2899e92a75cfbd3f3
Kristina <kristina.orl-va@bk.ru>
2023-05-17 13:20:39 +0300

    Time: 7 ms (22.50%), Space: 11.7 MB (60.19%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -4,25 +4,23 @@ public:
         make_heap(Cups.begin(), Cups.end());
         
         int Steps = 0;
-        while (true)
+        while (Cups.front() != 0)
         {
             const int FirstCup = Cups.front();
             pop_heap(Cups.begin(), Cups.end());
             Cups.pop_back();
             
             const int SecondCup = Cups.front();
-            pop_heap(Cups.begin(), Cups.end());
-            Cups.pop_back();
-            
-            if (FirstCup == 0 && SecondCup == 0)
+            if (SecondCup != 0)
             {
-                break;
+                pop_heap(Cups.begin(), Cups.end());
+                Cups.pop_back();
+                
+                Cups.push_back(SecondCup - 1);
+                push_heap(Cups.begin(), Cups.end());
             }
             
             Cups.push_back(FirstCup - 1);
-            push_heap(Cups.begin(), Cups.end());
-            
-            Cups.push_back(SecondCup != 0 ? SecondCup - 1 : 0);
             push_heap(Cups.begin(), Cups.end());
             
             Steps++;
