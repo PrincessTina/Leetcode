03b85823925b5e46bd1fc0bfc5bcb2a238518e5b
Kristina <kristina.orl-va@bk.ru>
2023-06-22 16:57:06 +0300

    Time: 45 ms (36.66%), Space: 73.3 MB (24.33%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* Node1, ListNode* Node2) {
+        stack<ListNode*> Stack1;
+        stack<ListNode*> Stack2;
+        
+        while (Node1 != nullptr)
+        {
+            Stack1.push(Node1);
+            Node1 = Node1->next;
+        }
+        
+        while (Node2 != nullptr)
+        {
+            Stack2.push(Node2);
+            Node2 = Node2->next;
+        }
+        
+        ListNode* Prev = nullptr;
+        int Rest = 0;
+        
+        // - - 2 1
+        // 7 2 4 3
+        
+        while (true)
+        {
+            ListNode* Current = nullptr;
+            int Sum = Rest;
+            Rest = 0;
+            
+            if (!Stack1.empty())
+            {
+                Current = Stack1.top();
+                Sum += Current->val;
+                
+                Stack1.pop();
+            }
+            
+            if (!Stack2.empty())
+            {
+                Sum += Stack2.top()->val;
+                
+                if (Current == nullptr)
+                {
+                    Current = Stack2.top();
+                }
+                
+                Stack2.pop();
+            }
+            
+            if (Sum >= 10)
+            {
+                Sum -= 10;
+                Rest = 1;
+            }
+            
+            if (Current == nullptr)
+            {
+                if (Sum > 0)
+                {
+                    return new ListNode(Sum, Prev);
+                }
+                
+                return Prev;
+            }
+            
+            Current->val = Sum;
+            Current->next = Prev;
+            Prev = Current;
+        }
+    }
+};
